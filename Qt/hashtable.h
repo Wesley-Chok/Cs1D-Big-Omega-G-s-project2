ee2305c37ac33933358b57e3d0092214e76e0cdb
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2020-05-10 23:48:39 -0700

    Update hashtable.h

--- Qt/hashtable.h
+++ Qt/hashtable.h
@@ -7,16 +7,43 @@ class Hash
     // Pointer to an array containing buckets
     QList<QString> *table;
 public:
+    /** \fn Constructor
+     * @param QObject *parent
+     */
     Hash();  // Constructor
 
     // inserts a key into hash table
+
+    /** \fn insertItem()
+     * This function will insert a key to the hashtable using int index and table[].
+     * @param QString user
+     * @param int x
+     * @return nothing returned
+     */
     void insertItem(QString user,int x);
 
     // hash function to map values to key
+    /** \fn insertItem()
+     * This function will construct and map the values to the keys.
+     * @param int x
+     * @return nothing returned
+     */
     int hashFunction(int x) {
         return (x % BUCKET);
     }
 
+    /** \fn insertItem()
+     * This function will find the pass for the hashtable and will return boolean false value at the end
+     * of the function
+     * @param QString user
+     * @return bool false
+     */
     bool foundpass(QString user);
+
+    /** \fn insertItem()
+     * This function will find the key for the hastable and will return -1 at the end of the function.
+     * @param QString user
+     * @return -1
+     */
     int findkey(QString user);
 };
